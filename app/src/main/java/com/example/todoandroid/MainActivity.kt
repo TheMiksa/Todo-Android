d86ca998e755660cc5bb3e71fa1af11cee8712ff
Michael Gritskov <michael.gritskov@gmail.com>
2023-04-02 20:39:58 +0200

    Added RecyclerView logic and Layout in MainActivity

--- app/src/main/java/com/example/todoandroid/MainActivity.kt
+++ app/src/main/java/com/example/todoandroid/MainActivity.kt
@@ -6,9 +6,18 @@ import com.example.todoandroid.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private val todoList = TodoManager.items
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val adapter = TodoListAdapter(todoList)
+        binding.rvTodoList.adapter = adapter
+        binding.btnAddItem.setOnClickListener {
+            TodoManager.setItem(TodoItem("Todo: ${Id.getId()}", "main_todo_list"))
+            println("${todoList.size}${todoList}")
+            adapter.notifyItemInserted(todoList.size - 1)
+        }
     }
 }
